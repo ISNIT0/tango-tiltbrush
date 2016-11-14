ed773bf25c1e2a7f02070e3cda694835d5a26eee
Joe Reeve <joe@wigwamm.com>
2016-11-14 11:24:44 +0000

    Initial Commit

---
+++ Assets/TouchManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchManager : MonoBehaviour {
+
+	public RectTransform colorHint;
+	public Shader lineShader;
+	private LineRenderer lineRenderer;
+	private int linePos = 0;
+
+	// Use this for initialization
+	void Start () {
+		var sizeDelta = colorHint.sizeDelta;
+		sizeDelta.x = Screen.width;
+		colorHint.sizeDelta = sizeDelta;
+		var tmpPos = colorHint.transform.position;
+		tmpPos.y = 0;
+		colorHint.transform.position = tmpPos;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.touchCount > 0) {
+			if (linePos >= 0) {
+				Vector3 position = Camera.main.transform.position + Camera.main.transform.forward * 1;
+				linePos += 1;
+				lineRenderer.SetVertexCount (linePos);
+				lineRenderer.SetPosition (linePos - 1, position);
+			} else {
+				linePos = 0;
+				float hue = (Input.touches [0].position.x / (float)Screen.width) * (float)360.0;
+				Color selectedColor = ColorUtils.ColorFromHSV (hue, 1, 1, 1);
+
+				float lineWidth = ((Input.touches [0].position.y + (float)40.0) / (float)Screen.height) * (float)0.3;
+
+				makeNewLine (selectedColor, lineWidth);
+			}
+		} else if(linePos >= 0) {
+			linePos = -1;
+		}
+	}
+
+	void makeNewLine(Color lineColor, float lineWidth = (float)0.05) {
+		GameObject item = new GameObject ();
+		lineRenderer = item.AddComponent<LineRenderer>();
+		lineRenderer.material = new Material(lineShader);
+		lineRenderer.material.color = new Color(0, 0, 0);
+		lineRenderer.SetColors(lineColor, lineColor);
+		lineRenderer.SetWidth (lineWidth, lineWidth);
+	}
+}
---
+++ Assets/TouchManagerJS.js
@@ -0,0 +1,45 @@
+﻿#pragma strict
+
+public var colorHint : RectTransform;
+public var lineShader : Shader;
+
+private var lineRenderer : LineRenderer;
+private var linePos : int = 0;
+
+private var colorUtils : ColorUtils = new ColorUtils();
+
+private var makeNewLine = function(lineColor:Color, lineWidth:float) {
+	lineWidth = lineWidth || 0.05;
+	var item : GameObject = new GameObject();
+	lineRenderer = item.AddComponent(LineRenderer) as LineRenderer;
+	lineRenderer.material = new Material(lineShader);
+	lineRenderer.material.color = new Color(0, 0, 0);
+	lineRenderer.SetColors(lineColor, lineColor);
+	lineRenderer.SetWidth (lineWidth, lineWidth);
+};
+
+function Start () {
+	colorHint.sizeDelta.x = Screen.width;
+	colorHint.transform.position.y = 0;
+}
+
+function Update () {
+	if (Input.touchCount > 0) {
+		if (linePos >= 0) {
+			var position : Vector3 = Camera.main.transform.position + Camera.main.transform.forward * 1;
+			linePos += 1;
+			lineRenderer.SetVertexCount(linePos);
+			lineRenderer.SetPosition(linePos - 1, position);
+		} else {
+			linePos = 0;
+			var hue : float = (Input.touches [0].position.x / Screen.width) * 360;
+			var selectedColor : Color = colorUtils.ColorFromHSV(hue, 1, 1, 1);
+
+			var lineWidth : float = ((Input.touches [0].position.y + 40) / Screen.height) * 0.3;
+
+			makeNewLine (selectedColor, lineWidth);
+		}
+	} else if(linePos >= 0) {
+		linePos = -1;
+	}
+}
